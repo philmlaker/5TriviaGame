daf7acf121a3cb5e115a8ae0191d322c1648dcbd
Philip Mlaker <philipmlaker@Philips-MBP.attlocal.net>
2016-11-20 10:50:24 -0600

    10:50AM Progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,37 @@
+// Timer
+
+var seconds_left = 10;
+var interval = setInterval(function() {
+    document.getElementById('timer_div').innerHTML = --seconds_left;
+
+    if (seconds_left <= 0)
+    {
+        document.getElementById('timer_div').innerHTML = 'Time is up!';
+
+        clearInterval(interval);
+        complete();
+    }
+}, 1000);
+
+
+
+
 // Checks for responses
-			function handleClick(){         
-				var amountCorrect = 0; 
-				var totalQuestions = 3;        
+
+var amountCorrect = 0; 
+var totalQuestions = 3; 
+			
+	function complete () {                  
+		        console.log("Correct Responses: " + amountCorrect + " out of " + totalQuestions);
+		        var a = parseInt(amountCorrect);
+		        var b = parseInt(totalQuestions);
+		        console.log("Incorrect Responses: " + (b-a) + " out of " + totalQuestions);
+		    		}
+
+
+
+
+			function handleClick(){                
 				for(var i = 1; i <= 45; i++) {
 		 			 var radios = document.getElementsByName('group'+i);
 		 			 for(var j = 0; j < radios.length; j++) {
@@ -11,9 +41,7 @@
 		   			 }
 		
 				  }
-				}                   
-		        console.log("Correct Responses: " + amountCorrect + " out of " + totalQuestions);
-		        var a = parseInt(amountCorrect);
-		        var b = parseInt(totalQuestions);
-		        console.log("Incorrect Responses: " + (b-a) + " out of " + totalQuestions);
+				} 
+
+		    		complete();
 		      }
